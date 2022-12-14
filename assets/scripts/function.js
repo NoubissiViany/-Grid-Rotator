ef65e0ec8af30f4d05bc9241c1c9031ba0467ca5
NNVG <g696607013@gmail.com>
2022-12-14 18:36:44 +0100

    Solve linter errors

--- assets/scripts/function.js
+++ assets/scripts/function.js
@@ -1,26 +1,26 @@
-let arr = [1, 4, 7, 8, 9, 6, 3, 2]
+const arr = [1, 4, 7, 8, 9, 6, 3, 2]
 
-let btn1 = document.getElementById('1')
-let btn2 = document.getElementById('2')
-let btn3 = document.getElementById('3')
-let btn4 = document.getElementById('4')
-let btn5 = document.getElementById('5')
-let btn6 = document.getElementById('6')
-let btn7 = document.getElementById('7')
-let btn8 = document.getElementById('8')
-let btn9 = document.getElementById('9')
+const btn1 = document.getElementById('1')
+const btn2 = document.getElementById('2')
+const btn3 = document.getElementById('3')
+const btn4 = document.getElementById('4')
+const btn5 = document.getElementById('5')
+const btn6 = document.getElementById('6')
+const btn7 = document.getElementById('7')
+const btn8 = document.getElementById('8')
+const btn9 = document.getElementById('9')
 
 let changedValue
 
-const clockwiseBut = document.querySelector('.rotate_btn')
-const antiClockwiseButs = document.querySelectorAll('.rotate_btns')
+const clockwiseBut = document.querySelector('.rotate-btn')
+const antiClockwiseButs = document.querySelectorAll('.rotate-btns')
 
-clockwiseBut.addEventListener ('click', function () {
+clockwiseBut.addEventListener ('click', function() {
   clockwise()
   repaint()
 })
 
-for (let but of antiClockwiseButs) {
+for (const but of antiClockwiseButs) {
   but.addEventListener('click', function () {
     anticlockwise()
     repaint()
@@ -28,8 +28,8 @@ for (let but of antiClockwiseButs) {
 }
 
 function clockwise () {
-    changedValue = arr.shift()
-    arr.push(changedValue)
+  changedValue = arr.shift()
+  arr.push(changedValue)
 }
 
 function anticlockwise () {
